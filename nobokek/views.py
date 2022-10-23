f00bd5f0b7818e7b3f28c7e94f549b738dc0c0d5
Irfan Satya Hendrasto <irfan.satya@ui.ac.id>
2022-10-23 21:56:44 +0700

    menambah fitur login

--- nobokek/views.py
+++ nobokek/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from nobokek.models import BarangWishlist
+...
+...
+@login_required(login_url='/nobokek/login/')
+def show_nobokek(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Kak Cinoy',
+        'last_login': request.COOKIES['last_login'],
+    }
+    return render(request, "nobokek.html", context)
 
-# Create your views here.
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('nobokek:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user) # melakukan login terlebih dahulu
+            response = HttpResponseRedirect(reverse("nobokek:show_nobokek")) # membuat response
+            response.set_cookie('last_login', str(datetime.datetime.now())) # membuat cookie last_login dan menambahkannya ke dalam response
+            return response
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('nobokek:login'))
+    response.delete_cookie('last_login')
+    return response
--- project_django/urls.py
+++ project_django/urls.py
@@ -12,6 +12,8 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    ipang
+    
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -19,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
+    path('nobokek/', include('nobokek.urls')),
 ]
