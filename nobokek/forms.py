f3a1a45b68920232450d711f742fb164ab87759c
Irfan Satya Hendrasto <irfan.satya@ui.ac.id>
2022-10-30 12:46:39 +0700

    tambah fitur kontak

--- nobokek/forms.py
+++ nobokek/forms.py
@@ -3,3 +3,8 @@ from django import forms
 class Stat(forms.Form):
     harga_barang = forms.CharField(label='Title')
     date = forms.DateField()
+
+class ContactForm(forms.Form):
+    nama = forms.CharField(label="Nama", max_length=150)
+    email = forms.EmailField(label="Email", widget=forms.Textarea)
+    masalah = forms.CharField(label="Masalah", max_length=1000)
--- nobokek/models.py
+++ nobokek/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -7,5 +9,12 @@ class BarangWishlist(models.Model):
     nama_barang = models.CharField(max_length=50)
     harga_barang = models.IntegerField()
     deskripsi = models.TextField()
-    date = models.TextField()
+    date = models.DateField(default=datetime.now)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+class ContactUs(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(default=datetime.now)
+    nama = models.CharField(max_length=150)
+    email = models.EmailField()
+    masalah =models.CharField(max_length=1000)
