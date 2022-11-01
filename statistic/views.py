f07d3a906856265096dcdf8da1fbbe25f828e018
MarcSalvadore <marc.salvadore@ui.ac.id>
2022-11-01 16:14:36 +0700

    adding statistic needs

--- statistic/views.py
+++ statistic/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render
+from django.shortcuts import redirect
+from django.http import HttpRequest
+from django.core import serializers
+
+import datetime
+from statistic.models import Stat
 
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+@login_required(login_url='/nobokek/login/')
+def show_statistic(request: HttpRequest):
+    data_money = Stat.objects.filter(user=request.user)
+    context = {
+    'list_data' : data_money,
+    }
+    return render(request, "statistic.html", context)
+
+@login_required(login_url='/nobokek/login/')
+def show_json(request):
+    money = Stat.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize('json', money), content_type='application/json')
