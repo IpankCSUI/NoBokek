563bb0dd58ada634c65b4896ddea1703403610aa
Irfan Satya Hendrasto <irfan.satya@ui.ac.id>
2022-12-11 20:54:50 +0700

    up auth flutter

--- project_django/settings.py
+++ project_django/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'forum',
     'add',
     'report',
-    'statistic'
+    'statistic',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ALLOW_ALL_ORIGINS=True
+
+CORS_ALLOW_CREDENTIALS=True
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
 
 ROOT_URLCONF = 'project_django.urls'
 
